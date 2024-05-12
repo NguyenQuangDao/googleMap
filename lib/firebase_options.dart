d0e2b4b488e91eefa044a36cba37e03eae76093d
NguyenQuangDao <anhdao2292003cxz@gmail.com>
2024-05-12 20:42:42 +0700

    first commit

---
+++ lib/firebase_options.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
+
+FirebaseOptions firebaseOptions = const FirebaseOptions(
+  apiKey: 'AIzaSyBBb_8fa9KUrEDMMMckHwpiREmEVNcXVHA',
+  appId: '1:216484836183:android:f5759bff45062d2e314412',
+  messagingSenderId: '216484836183',
+  projectId: 'nckh2024ggmaplidar',
+  storageBucket: 'nckh2024ggmaplidar.appspot.com',
+);
